26a3c6801e1a5337e4e5f2973258262aacd81fd9
Gleb Kuzmin <41387215+glkuzi@users.noreply.github.com>
2019-01-06 20:33:18 +0300

    Added model and predict

--- Predict.py
+++ Predict.py
@@ -11,33 +11,51 @@ import json
 from DataGenerator import *
 
 
-def main():
-    model_name = '1_500_81_1_500_5_with_regs_0_6overlapPool_e-5_decay5e-4_abs_norm_peak_norm.600-1.36.hdf5'#'wide_samples_250_long_without_regs.13100-1.26.hdf5'
-    size = 500
+def accuracy_test(model_name, test=test):
+    dg_test = DataGenerator(test)
+    Num = 10
+    model = load_model(model_name)
+    acc = []
+    for i in range(Num):
+        length = dg_test.__len__()
+        for j in range(length):
+            X, Y = dg_test.__getitem__(j)
+            Y_pred = model.predict_classes(X)
+            Y_not = []
+            for y in Y[0]:
+                Y_not.append(list(y).index(1.))
+            Y_diff = Y_pred[0] - np.array(Y_not)
+            count = 0
+            for y in Y_diff:
+                if y == 0:
+                    count += 1
+            acc.append(count / len(Y_diff))
+        dg_test.on_epoch_end()
+    return np.mean(acc)
+
+
+def predict_single_sample(model_name, test=test):
     sig, json_arr = mixGenerate(json.load(open(test)), duration=5,
                                               shuffle=True)
     matrix = stft(sig)
-    print(0)
-    matrix0 = matrix#np.array(np.log(matrix / np.abs(matrix) + 1e-5))
-    print(matrix0.shape)
     target = notation(sig, json_arr)
     model = load_model(model_name)
-    res = []
-    for i in range(1, matrix.shape[0] // size):
-        t = []
-        t.append(matrix0[(i - 1) * size:i * size, :])
-        res.append(model.predict_classes(np.array(t)))
-    matr = []
-    matr.append(matrix0)
-    res = model.predict_classes(np.array(matr))
-    '''
-    final_res = []
-    for x in res:
-        final_res = final_res + list(x[0])
-    print(final_res)
-    '''
-    print(res)
-    print(target)
+    res = model.predict_classes(np.array([matrix]))
+    print('Predicted notation:', res)
+    print('Real notation:', target)
+    diff = res[0] - np.array(target)
+    count = 0
+    for y in diff:
+        if y == 0:
+            count += 1
+    acc = count / len(diff)
+    print('Accuracy:', acc)
+
+
+def main():
+    model_name = '1_500_81_1_500_5_with_regs_0_6overlapPool_e-5_decay5e-4_abs_norm_peak_norm.600-1.36.hdf5'
+    #print(accuracy_test(model_name='1_500_81_1_500_5_with_regs_0_6overlapPool_e-5_decay5e-4_abs_norm_peak_norm.600-1.36.hdf5'))
+    predict_single_sample(model_name)
     return 0
 
 
